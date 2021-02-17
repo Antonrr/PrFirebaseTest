613b316b5fce513e3530683b7b11c0a9b75e050b
Anton Rassadin <anton@rassadin.net>
2021-02-17 10:33:36 +0300

    add Firebase module dependency

--- Source/FirebaseTest/FirebaseTest.Build.cs
+++ Source/FirebaseTest/FirebaseTest.Build.cs
@@ -8,7 +8,7 @@ public class FirebaseTest : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 	
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "PrFirebase" });
 
 		PrivateDependencyModuleNames.AddRange(new string[] {  });
 
