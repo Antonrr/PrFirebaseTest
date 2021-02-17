04b2b5ee9c8c7ae99f7a9ff22516b327563f1b88
Anton Rassadin <anton@rassadin.net>
2021-02-17 10:11:35 +0300

    Init repository

---
+++ Source/FirebaseTest/FirebaseTestGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FirebaseTestGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FIREBASETEST_API AFirebaseTestGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
